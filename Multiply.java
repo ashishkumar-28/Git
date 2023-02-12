84b8eb97009099e9b99aac5e4f4c7b7d1b28f474
Ashish Kumar <ak2831637@gmail.com>
2023-02-12 22:46:50 +0530

    all

--- Multiply.java
+++ Multiply.java
@@ -2,7 +2,7 @@ public class Multiply {
     public static void main(String[] args) {
         int a=6;
         int b=9;
-        System.out.println(a*b);
+        System.out.println("mult is"+a*b);
     }
     
 }
